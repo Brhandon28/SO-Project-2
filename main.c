e68ae7e6b097f395c899e29a54130262636dfc53
Brhandon Palomo <brhandon28@gmail.com>
2025-03-02 00:27:02 -0400

    Refactor main function to utilize leerArchivos for file processing

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "leerArchivos.h"
 
-int main() {
-    
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    leerArchivos(argc, argv);
     return 0;
 }
